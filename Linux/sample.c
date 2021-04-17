168f865b9fd6f5ac32073f74ad11351a9c828391
ECT <enescan58turko@gmail.com>
2021-04-17 15:55:19 +0300

    v1.0.10
    DERS: 12
    Preprocessing işlemini gösteren güzel bir örnek

--- Linux/sample.c
+++ Linux/sample.c
@@ -1,14 +1,14 @@
 #include "stdio.h"
-#define MAX   100
 
 
-int main(int argc, char *argv[])
+int main(void)
 {
-	double result;
-
-	result = MAX;
+	int x =
 	
-	printf("Test %f\n", result);
+	#include "test.h"
+	;
+	
+	printf("Test %d\n", x);
 	
 	return 0;
 }
@@ -18,60 +18,40 @@ int main(int argc, char *argv[])
 
 
 /****************************************************** Others
- *  KOD CALSTIRMA ORNEGI:
- *  gcc -E sample.c
+ * KOD CALSTIRMA ORNEGI:
+ * gcc -o sample sample.c 
+ * ./sample
  *  
 */
 
 /************************************************ CODE PARTICLES
- * gcc -E sample.c yapılarak derlemeden ve linklemeden önceki, kodun preprocessor kısmı gözlemlenebilmektedir. Bu kısımda # ile belirtilen yerler
- * uygun değerlerle doldurulup farklı bir temp dosyaya yazılıp bu dosya silinip diğer temp dosyanın ismi değiştirilerek daha sonra derleme ve linkelem 
- * işlemi yapılarak tamamlanır. 
- * Preprocessor işlemi sadece #define için değildir. Örneğin kütüphaneyi #include "stdio.h" include edersek, preprocessor derleme yapmadan önce stdio.h
- * kütüphanesinin tamamını kopyalar ve kodun yukarısını yapıştırır. 
- * Deneme için aşağıdaki kod belirtilen şekilde çalıştırılabilir. Fazla satır olduğu için yapmadım.;
-
-gcc -E sample.c
-
-#include "stdio.h"
-#define MAX   100
-
-
-int main(int argc, char *argv[])
-{
-	double result;
-
-	result = MAX;
-	
-	printf("Test %f\n", result);
-	
-	return 0;
-} 
+ * Bu örnekle birlikte; include edilen #include "test.h" dosyanin, preprocessor tarafından olduğu gibi
+ * koda yazıldığı açıkça görülmektedir.
+ * 
+ * test.h içerisinde 10 yazmaktadır.
 
  ******************************************************************************************************************************************************
-ÇIKTI - 1
-
-ect@ect-virtual-machine:~/Desktop/sample$ gcc -E sample.c
-# 1 "sample.c"
-# 1 "<built-in>"
-# 1 "<command-line>"
-# 31 "<command-line>"
-# 1 "/usr/include/stdc-predef.h" 1 3 4
-# 32 "<command-line>" 2
-# 1 "sample.c"
-
-
-int printf(const char *format, ...);
-
-int main(int argc, char *argv[])
+ * Kod bu şekilde çalıştırılarak preprocessorun ne yazdığı gösterilebilir.
+ * gcc -E sample.c 
+ * ./sample
+ * 
+ * ÇIKTI : 
+ ***************************
+ ...
+ int main(void)
 {
- double result;
+ int x =
 
- result = 100;
+# 1 "test.h" 1
+10
+# 9 "sample.c" 2
+ ;
 
- printf("Test %f\n", result);
+ printf("Test %d\n", x);
 
  return 0;
 }
+
+
 ******************************************************************************************************************************************************
 */
