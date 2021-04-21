9e50dcc575f61803a508c86800433eb95c178557
ECT <enescan58turko@gmail.com>
2021-04-21 12:30:59 +0300

    v1.0.16
    DERS 17:  Linux
    
    Linux errno -> strerror

--- Linux/sample.c
+++ Linux/sample.c
@@ -2,7 +2,7 @@
 #include "stdlib.h"									/* EXIT_FAILURE icin*/
 #include "fcntl.h"									/* open icin */
 #include "errno.h"									/* errno icin */
-
+#include "string.h"									/* strerror icin */
 
 
 int main(void)
@@ -12,7 +12,7 @@ int main(void)
 	
 	if ( (result = open("test.txt", O_RDONLY)) == -1)
 	{
-		fprintf(stderr, "Open fail: %d\n", errno);
+		fprintf(stderr, "Open fail: %s\n", strerror(errno));
 		exit(EXIT_FAILURE);
 	}
 
@@ -35,7 +35,10 @@ int main(void)
 	barasili olmasi durumunda ise "0" degerini dondurur. Fakat bazı adres donen POSIX fonksiyonlari hata durumunda NULL da donebilmektedir.
 
 	Bu durumda "open" fonksiyonu kendi icerisinde "errno" degiskenini doldurur. Doldurulan bu sayısal deger bir hata koduna denk gelmektedir.
-	Bu hata kodu sonraki ornekte yazidirilacaktir.  
+	Bu hata kodu ise "strerror(errno)" fonksiyonuyla yapilmaktadir. Bu fonksiyon errno giris parametresini alarak ilgili hata kodunun adresini
+	doner, biz de bu adresi string olarak yazdiririz.
+
+
 
 ******************************************************************************************************************************************************
 */
