3739296d70bbfb719ef0a4b41cc5b93f06bc2caf
ECT <enescan58turko@gmail.com>
2021-04-28 18:44:18 +0300

    v1.1.14 : Windows Karakter Encoidng- W2
    DERS 22:  Windows
    
    Windows Karakter Encoidng islemleri

--- Windows/Sample/Sample.c
+++ Windows/Sample/Sample.c
@@ -1,5 +1,4 @@
 ﻿#include "stdio.h"
-#include "stdlib.h"
 #include "windows.h"
 
 
@@ -9,64 +8,41 @@ void ExitSys(LPCSTR lpszMsg);
 
 int main(void)
 {
-	HANDLE		 hStdOut;
-	const char   buf[]     = {"Ali"};
-	DWORD		 dwWritten;
-	COORD		 coord;
-	WORD		 wAttr[3];
+	/* Ekrana bir yazi yazidirlmadan once veya bir yazi okunmadan once Console ve
+	   Editor karakter encodinglerinin ayni almasi gerekmektedir. 
+	   Bu ornekle beraber PC karakter kodu UTF8-65001 olarak yapilmistir. 
+	   
+	   Calisilan osyanin editordeki karakter kodu da Save as->Save/Secenekler->Save with encoding diyerek
+	   degistirilebilir. Editorun komple degistirildigi bir senecek de mevcuttur buyuk ihtimalle.
+	   
+	   Ama onemli olan sey, editor ile consolun aynı karakter encoding degerine sahip olmasi gerekmektedir.
 
-	if ((hStdOut = GetStdHandle(STD_OUTPUT_HANDLE)) == INVALID_HANDLE_VALUE)
-		ExitSys("GetStdHandle");
+	   SetConsoleOutputCP(...) ile de konsol karakter encoing degeri gecici olarak değiştirilebilmektedir.
 
-	/* StdOutHandle'i ile belirtilen bufferin belirtilen byte kadar ekrana yazdirilmasi */
-	if( !(WriteConsole(hStdOut, buf, (DWORD)strlen(buf), &dwWritten, NULL)) )
-		ExitSys("WriteConsole");;
+	   Karakter tabloları;
 
-	coord.X = 0;
-	coord.Y = 0;
+	   1Byte'lık ve 2Byte'lık olmak uzere 2'ye ayrılır.
 
-	wAttr[0] = BACKGROUND_BLUE | FOREGROUND_RED;
-	wAttr[1] = FOREGROUND_RED  | FOREGROUND_BLUE;
-	wAttr[2] = BACKGROUND_BLUE | FOREGROUND_GREEN;
+		1Byte'lık tablolarda karakterler 1Bytedır ve birçok farklı region vs. için çeşitlidir.
+		2Byte'lık tablolarda karakterler 2Bytedır. Unicode olarak da denmektedir. Bütün dünyada aynıdır.
+		Unicode tabloda dünyanın bütün karakterleri vardır.
 
-	/* StdOutHandle'i ile belirtilen COORdinattan baslanarak belirtilen byte kadar 
-		tasarlanan karakter attribute dizisine gore islenir, boyanir vs. */
-	WriteConsoleOutputAttribute(hStdOut, wAttr, (DWORD)strlen(buf), coord, &dwWritten);
+		UTF8 Tablosu ise sıkıştırılmış Unicode tablo anlamındadır. Bu tabloda ASCII karakterler 1 byte ile kodlanir. 
+		Eger deger ASCII dışına geliyorsa bu karakter 2byte, 3byte vs kodlarınır. Bu kodlama biçimine "Multibyte kodlama" denmektedir.
+			Ornegin; 
+		
+		char text[] = "ağ";
+		printf("%u", strlen(text));
+		----> 3Byte
 
-	coord.X = 10;
-	coord.Y = 20;
-	/* StdOutHandle'i ile Cursor(Imlec) yeri belirlenen koordinatlara goturulur */
-	SetConsoleCursorPosition(hStdOut, coord);
+		C Standartlarına gore ''(Orn 'x') icerisinde bir karakter alınırsa 1Byte yer kaplaması zorunlu değildir ve bu int türündendir. 
+		Fakat cpp'da ''(Orn 'x') icerisinde bir karakter alınırsa 1Byte yer kaplar ve türü char'dır. 
+	   */
 
-	if (!(WriteConsole(hStdOut, buf, (DWORD)strlen(buf), &dwWritten, NULL)))
-		ExitSys("WriteConsole");;
-
-	WriteConsoleOutputAttribute(hStdOut, wAttr, (DWORD)strlen(buf), coord, &dwWritten);
+	printf("%c", 'Ğ');
 
 	return 0;
 }
-
-
-
-void ExitSys(LPCSTR lpszMsg)
-{
-	DWORD dwLastError = GetLastError();
-	PTSTR lpszErr;
-
-	if (FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM, NULL, dwLastError,
-		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (PTSTR)&lpszErr, 0, NULL)) {
-		printf("%s: ", lpszMsg);
-
-		for (int i = 0; lpszErr[i] != '\n'; i++)
-			printf("%c", lpszErr[i]);
-		printf("\n");
-
-		LocalFree(lpszErr);
-	}
-
-	exit(EXIT_FAILURE);
-}
-
 
 
 
@@ -76,84 +52,11 @@ void ExitSys(LPCSTR lpszMsg)
 
 /****************************************************** Others
 	-----------------CALISTIRMA ORNEGI
-	SetFilePointer fonksiyonu ile okunan "Test.txt" dosyasinda belirlenen deger kadar dosya göstericisi kaydirilmistir ve sonrasinda dosyadan okuma yapilmistir.
 
+	CMD CODPAGE DEGISTIRME ORNEGI
+	https://stackoverflow.com/questions/7432545/change-codepage-in-cmd-permanently/30100565
 
-	--------- WriteConsole  ---------
-	https://docs.microsoft.com/en-us/windows/console/writeconsole
-
-	BOOL WINAPI WriteConsole(
-								_In_             HANDLE  hConsoleOutput,
-								_In_       const VOID    *lpBuffer,
-								_In_             DWORD   nNumberOfCharsToWrite,
-								_Out_opt_        LPDWORD lpNumberOfCharsWritten,
-								_Reserved_       LPVOID  lpReserved
-							);
-
-
-	Writes a character string to a console screen buffer beginning at the current cursor location.
-
-	Parameters:
-	----------	
-	hConsoleOutput [in]
-	A handle to the console screen buffer. The handle must have the GENERIC_WRITE access right. For more information, see Console Buffer Security and Access Rights.
-	
-	lpBuffer [in]
-	A pointer to a buffer that contains characters to be written to the console screen buffer. This is expected to be an array of either char for WriteConsoleA or wchar_t for WriteConsoleW.
-	
-	nNumberOfCharsToWrite [in]
-	The number of characters to be written. If the total size of the specified number of characters exceeds the available heap, the function fails with ERROR_NOT_ENOUGH_MEMORY.
-	
-	lpNumberOfCharsWritten [out, optional]
-	A pointer to a variable that receives the number of characters actually written.
-
-	lpReserved Reserved; must be NULL.
-	
-	Return value:
-	------------
-	If the function succeeds, the return value is nonzero.
-	If the function fails, the return value is zero. To get extended error information, call GetLastError.
-
-	
-	--------- WriteConsoleOutputAttribute  ---------
-	https://docs.microsoft.com/en-us/windows/console/writeconsoleoutputattribute
-
-	BOOL WINAPI WriteConsoleOutputAttribute(
-											 _In_        HANDLE  hConsoleOutput,
-											 _In_  const WORD    *lpAttribute,
-											 _In_        DWORD   nLength,
-											 _In_        COORD   dwWriteCoord,
-											 _Out_       LPDWORD lpNumberOfAttrsWritten
-										   );
-
-	--> StdOutHandle'i ile belirtilen COORdinattan baslanarak belirtilen byte kadar tasarlanan karakter attribute dizisine gore islenir, boyanir vs.
-	Onemli nokta, lpAttribute dizisi her bir karakter icin tek tek belirtilmelidir. Karakter bazli calisir.
-
-	Copies a number of character attributes to consecutive cells of a console screen buffer, beginning at a specified location.
-
-	Parameters:
-	----------
-	hConsoleOutput [in]
-	A handle to the console screen buffer. The handle must have the GENERIC_WRITE access right. For more information, see Console Buffer Security and Access Rights.
-	
-	lpAttribute [in]
-	The attributes to be used when writing to the console screen buffer. For more information, see Character Attributes.
-	
-	nLength [in]
-	The number of screen buffer character cells to which the attributes will be copied.
-	
-	dwWriteCoord [in]
-	A COORD structure that specifies the character coordinates of the first cell in the console screen buffer to which the attributes will be written.
-	
-	lpNumberOfAttrsWritten [out]
-	A pointer to a variable that receives the number of attributes actually written to the console screen buffer.
-
-	Return value:
-	------------
-	If the function succeeds, the return value is nonzero.
-	If the function fails, the return value is zero. To get extended error information, call 
-
-
+	CMD CODPAGE DEGISTIRME UTF8 - 65001 yapilmistir.
 
  ******************************************************************************************************************************************************
 
