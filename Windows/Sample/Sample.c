dfc64f0ef03599f6af83f49ee543c468171020e3
ECT <enescan58turko@gmail.com>
2021-05-02 14:38:09 +0300

    v1.1.20 : Yardimci Dosya Fcn, CreateDirectory, RemoveDirectory- W5
    DERS 24:  Windows
    
    CreateDirectory ve RemoveDirectory fonksiyonlari

--- Windows/Sample/Sample.c
+++ Windows/Sample/Sample.c
@@ -9,22 +9,22 @@ void ExitSys(LPCSTR lpszMsg);
 
 int main(void)
 {
-	HANDLE hFile;
-	DWORD  dwFileSize;
+	
+	/* Olmayan bir dizin yaratilir. */
+	if (!CreateDirectory("dirTest", NULL))
+		ExitSys("CreateDirectory");
 
-	/* Dosya Read ve Open Existing modda acilir, handle degeri elde edilir. */
-	if( (hFile = CreateFile("Test.txt", GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL)) == INVALID_HANDLE_VALUE )
-		ExitSys("CreateFile");
+	/* Yukaridaki PathName'in uzun hali
+	if (!CreateDirectory("C:\\Users\\enesc\\Masaüstü\\GitKrakenRepo\\CSystem_Sample\\CSystem\\Windows\\Sample\\dirTest", NULL))
+		ExitSys("CreateDirectory");
+	*/
 
-	/* handle degeri alinan dosya, GetFileSize fonksiyonuna girlir. 
-		 Eger okunacak dosyanin uzunlugu biliniyorsa ve LPDWORD degilse 2.parametre NULL gecilebilir. Return degeri size degeri olacaktir.
-	     Eger 2.parametre NULL gecilmezse, return degerine alcak 4 byte, 2. parametreye yuksek 4 byte'lik degeri yazilir.  */
-	if( (dwFileSize=GetFileSize(hFile, NULL)) == INVALID_FILE_SIZE)
-		ExitSys("GetFileSize");
-		
-	/* Dosya uzunlugu yazdirilir. */
-	printf("File size: %lu\n", dwFileSize);
 
+	/* Bos olan bir dizin silinir. */
+	if (!RemoveDirectory("dirTest"))
+		ExitSys("RemoveDirectory");
+
+	printf("Dizin yaratildi ve silindi. \n");
 	return 0;
 }
 
@@ -56,49 +56,47 @@ void ExitSys(LPCSTR lpszMsg)
 
 
 /****************************************************** Others
-*   Test.txt dosyasinin Karakter encodingi = Codepage 1252
-*   Eger karakter encodingi değiştirilirse, örn UTF-8 vs yapilirsa
-*   karakter boyutu değişeceğinden dosya boyutu da değişir.
 
-	Test.txt içeriği = 123456789 
-	GetFileSize ile dosyanın okunup ekrana bastırılan uzunluğu ise 9 Byte.
+	Verilen PathName'e sahip bir directory(Dosya) varsa hata verir. Verilen PathName'in kullanilmamis olmasi lazim.
 
+ --------------------------------------------------------------------- C r e a t e  D i r e c t o r y ---------------------------------------------------------- 
 
-	!!!
-	GetFileSize fonksiyonu açılmış bir dosyanın uzunluğunu elde etmek için kullanılmaktadır. Bu fonksiyonun birinci parametresi açılmış dosyanın handle değerini, 
-	ikinci parametresi ise dosya uzunluğunun yüksek anlamlı 4 byte'lık kısmının yerleştirileceği DWORD nesnenin adresini almaktadır. 
-	İkinci parametre NULL geçilebilir. GetFileSize fonksiyonu başarısızlık durumunda INVALID_FILE_SIZE özel değerine geri döner. 
-	Ancak bu INVALID_FILE_SIZE sembolik sabiti 0xFFFFFFFF değerine sahip olduğu için ve bu değer de geçerli bir dosya uzunluğu olabildiği için programcının 
-	ayrıca fonksiyonun başarısını GetLastError fonksiyonuyla belirlemesi gerekmektedir. 
-		Yani eğer fonksiyon INVALID_FILE_SIZE değerine geri dönmüşse programcının GetLastError fonksiyonunu çağırması gerekir. GetLastError eğer NO_ERROR değerini verirse fonksiyon başarısız olmamıştır. 
-		Dosya gerçekten 0xFFFFFFFF uzunluğundadır
-	!!!
-
- --------------------------------------------------------------------- G e t  F i l e  S i z e ---------------------------------------------------------- 
-
-	Retrieves the size of the specified file, in bytes.
+	Verilen Path Name'de yeni bir dizin yaratir.
 	
-	DWORD GetFileSize(
-					  HANDLE  hFile,
-					  LPDWORD lpFileSizeHigh
-					);
+	BOOL CreateDirectory(
+						  LPCTSTR lpPathName,
+						  LPSECURITY_ATTRIBUTES lpSecurityAttributes
+						);
+
 	
 	PARAM:
 	-----
-	hFile               --> A handle to the file.
-	lpFileSizeHigh      --> A pointer to the variable where the high-order doubleword of the file size is returned. 
-							This parameter can be NULL if the application does not require the high-order doubleword.
+	lpPathName               --> The path of the directory to be created.
+	lpSecurityAttributes     --> A pointer to a SECURITY_ATTRIBUTES structure. The lpSecurityDescriptor member of the structure specifies a security descriptor for the new directory. 
+	                             If lpSecurityAttributes is NULL, the directory gets a default security descriptor.
 
 	RETURN:
 	------
-	If the function succeeds, the return value is the low-order doubleword of the file size, and, if lpFileSizeHigh is non-NULL, 
-	the function puts the high-order doubleword of the file size into the variable pointed to by that parameter.
+	If the function succeeds, the return value is nonzero.
+	If the function fails, the return value is zero. To get extended error information, call GetLastError. 
 
-	If the function fails and lpFileSizeHigh is NULL, the return value is INVALID_FILE_SIZE. To get extended error information, 
-	call GetLastError. When lpFileSizeHigh is NULL, the results returned for large files are ambiguous, and you will not be able to determine the actual size of the file. 
-	It is recommended that you use GetFileSizeEx instead.
+	 --------------------------------------------------------------------- R e m o v e  D i r e c t o r y ----------------------------------------------------------
 
-	If the function fails and lpFileSizeHigh is non-NULL, the return value is INVALID_FILE_SIZE and GetLastError will return a value other than NO_ERROR.
+	Dizinin ici bossa dizini siler.
+	Deletes an existing empty directory.
+
+	BOOL RemoveDirectory(
+						  LPCSTR lpPathName
+						);
+
+	PARAM:
+	-----
+	lpPathName               --> The path of the directory to be removed. This path must specify an empty directory, and the calling process must have delete access to the directory.
+
+	RETURN:
+	------
+	If the function succeeds, the return value is nonzero.
+	If the function fails, the return value is zero. To get extended error information, call GetLastError.
 
 ******************************************************************************************************************************************************
 */
