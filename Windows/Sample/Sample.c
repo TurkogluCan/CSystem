eac584292bfe5b4a62328926051f632d6a034653
ECT <enescan58turko@gmail.com>
2021-05-02 13:56:32 +0300

    v1.1.19 : Yardimci Dosya Fcn, GetFileSize - W4
    DERS 23:  Windows
    
    GetFileSize fonksiyonu

--- Windows/Sample/Sample.c
+++ Windows/Sample/Sample.c
@@ -9,12 +9,21 @@ void ExitSys(LPCSTR lpszMsg);
 
 int main(void)
 {
+	HANDLE hFile;
+	DWORD  dwFileSize;
 
-	/* mest.txt dosyasinin dizin bilgileri movedMest.txt olarak degistirilir. */
-	if ( !MoveFile("mest.txt", "movedMest.Txt") )
-		ExitSys("MoveFile");
+	/* Dosya Read ve Open Existing modda acilir, handle degeri elde edilir. */
+	if( (hFile = CreateFile("Test.txt", GENERIC_READ, 0, NULL, OPEN_EXISTING, 0, NULL)) == INVALID_HANDLE_VALUE )
+		ExitSys("CreateFile");
 
-	printf("OK\n");
+	/* handle degeri alinan dosya, GetFileSize fonksiyonuna girlir. 
+		 Eger okunacak dosyanin uzunlugu biliniyorsa ve LPDWORD degilse 2.parametre NULL gecilebilir. Return degeri size degeri olacaktir.
+	     Eger 2.parametre NULL gecilmezse, return degerine alcak 4 byte, 2. parametreye yuksek 4 byte'lik degeri yazilir.  */
+	if( (dwFileSize=GetFileSize(hFile, NULL)) == INVALID_FILE_SIZE)
+		ExitSys("GetFileSize");
+		
+	/* Dosya uzunlugu yazdirilir. */
+	printf("File size: %lu\n", dwFileSize);
 
 	return 0;
 }
@@ -47,33 +56,49 @@ void ExitSys(LPCSTR lpszMsg)
 
 
 /****************************************************** Others
- 
+*   Test.txt dosyasinin Karakter encodingi = Codepage 1252
+*   Eger karakter encodingi değiştirilirse, örn UTF-8 vs yapilirsa
+*   karakter boyutu değişeceğinden dosya boyutu da değişir.
+
+	Test.txt içeriği = 123456789 
+	GetFileSize ile dosyanın okunup ekrana bastırılan uzunluğu ise 9 Byte.
+
+
 	!!!
-	Taşıma demek, dataları kopyalamak değil, dizin girişini silerek başka bir dizin içerisinde burayı referans eden bir dizin yaratmak demek.
-	Dosyanın erişmek için gereken path’i değiştirilir, dosya içeriği değiştirilmez. Aslında dosyanin isim değişikliği de dosya taşımayla yapılmaktadır. 
+	GetFileSize fonksiyonu açılmış bir dosyanın uzunluğunu elde etmek için kullanılmaktadır. Bu fonksiyonun birinci parametresi açılmış dosyanın handle değerini, 
+	ikinci parametresi ise dosya uzunluğunun yüksek anlamlı 4 byte'lık kısmının yerleştirileceği DWORD nesnenin adresini almaktadır. 
+	İkinci parametre NULL geçilebilir. GetFileSize fonksiyonu başarısızlık durumunda INVALID_FILE_SIZE özel değerine geri döner. 
+	Ancak bu INVALID_FILE_SIZE sembolik sabiti 0xFFFFFFFF değerine sahip olduğu için ve bu değer de geçerli bir dosya uzunluğu olabildiği için programcının 
+	ayrıca fonksiyonun başarısını GetLastError fonksiyonuyla belirlemesi gerekmektedir. 
+		Yani eğer fonksiyon INVALID_FILE_SIZE değerine geri dönmüşse programcının GetLastError fonksiyonunu çağırması gerekir. GetLastError eğer NO_ERROR değerini verirse fonksiyon başarısız olmamıştır. 
+		Dosya gerçekten 0xFFFFFFFF uzunluğundadır
 	!!!
 
- --------------------------------------------------------------------- M O V E  F I L E ---------------------------------------------------------- 
+ --------------------------------------------------------------------- G e t  F i l e  S i z e ---------------------------------------------------------- 
 
-	Bir dosyayı yeni bir konuma taşır.
+	Retrieves the size of the specified file, in bytes.
 	
-	BOOL MoveFile(
-				  LPCTSTR lpExistingFileName,
-				  LPCTSTR lpNewFileName
-				);
+	DWORD GetFileSize(
+					  HANDLE  hFile,
+					  LPDWORD lpFileSizeHigh
+					);
 	
 	PARAM:
 	-----
-	lpExistingFileName      --> The current name of the file or directory on the local computer. Must be exist.
-	lpNewFileName           --> The new name for the file or directory. The new name must not already exist. 
-								A new file may be on a different file system or drive.
-								A new directory must be on the same drive.
-								Yani dosya başka bir drive(disk)'e taşınabilir fakat bir dizin başka bir drive'a taşınamaz.
+	hFile               --> A handle to the file.
+	lpFileSizeHigh      --> A pointer to the variable where the high-order doubleword of the file size is returned. 
+							This parameter can be NULL if the application does not require the high-order doubleword.
 
 	RETURN:
 	------
-	If the function succeeds, the return value is nonzero.
-	If the function fails, the return value is zero. To get extended error information, call "GetLastError".
+	If the function succeeds, the return value is the low-order doubleword of the file size, and, if lpFileSizeHigh is non-NULL, 
+	the function puts the high-order doubleword of the file size into the variable pointed to by that parameter.
+
+	If the function fails and lpFileSizeHigh is NULL, the return value is INVALID_FILE_SIZE. To get extended error information, 
+	call GetLastError. When lpFileSizeHigh is NULL, the results returned for large files are ambiguous, and you will not be able to determine the actual size of the file. 
+	It is recommended that you use GetFileSizeEx instead.
+
+	If the function fails and lpFileSizeHigh is non-NULL, the return value is INVALID_FILE_SIZE and GetLastError will return a value other than NO_ERROR.
 
 ******************************************************************************************************************************************************
 */
